22ee086aee1b93ef4ea8cbf076cfb6db949d5f11
Vladimir Burdukov <chippcheg@gmail.com>
2020-04-30 11:28:16 +0300

    Removed delay, added on_event callback

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use rumble::api::{Central, CentralEvent, Peripheral, UUID};
 use rumble::bluez::manager::Manager;
-use std::thread;
-use std::time::Duration;
+use std::sync::Arc;
+// use std::thread;
+// use std::time::Duration;
 
 fn main() {
     let manager = Manager::new().unwrap();
@@ -15,46 +16,27 @@ fn main() {
     adapter = manager.up(&adapter).unwrap();
 
     // connect to the adapter
-    let central = adapter.connect().unwrap();
+    let central = Arc::from(adapter.connect().unwrap());
 
     // start scanning for devices
     central.start_scan().unwrap();
     // instead of waiting, you can use central.on_event to be notified of
     // new devices
 
-    thread::sleep(Duration::from_secs(2));
+    let clone = Arc::clone(&central);
+    central.on_event(Box::new(move |event| match event {
+        CentralEvent::DeviceDiscovered(addr) => {
+            let peripheral = clone.peripheral(addr).unwrap();
 
-    // find the device we're interested in
-    let temp = central
-        .peripherals()
-        .into_iter()
-        .find(|p| {
-            p.properties()
+            if peripheral
+                .properties()
                 .local_name
                 .iter()
                 .any(|name| name.contains("MJ_HT_V1"))
-        })
-        .unwrap();
-
-    // connect to the device
-    temp.connect().unwrap();
-
-    // discover characteristics
-    temp.discover_characteristics().unwrap();
-
-    // find the characteristic we want
-    let chars = temp.characteristics();
-    let cmd_char = chars.iter().find(|c| c.uuid == UUID::B16(0x0024)).unwrap();
-
-    let vec = temp.read(&cmd_char).unwrap();
-
-    println!("{}", String::from_utf8(vec).unwrap());
-
-    // // dance party
-    // let mut rng = thread_rng();
-    // for _ in 0..20 {
-    //     let color_cmd = vec![0x56, rng.gen(), rng.gen(), rng.gen(), 0x00, 0xF0, 0xAA];
-    //     light.command(&cmd_char, &color_cmd).unwrap();
-    //     thread::sleep(Duration::from_millis(200));
-    // }
+            {
+                println!("found")
+            }
+        }
+        _ => (),
+    }));
 }
